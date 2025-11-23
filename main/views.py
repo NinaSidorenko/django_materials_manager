b85c2fc74c7e858c475d83273f300836382a0716
NinaSidorenko <ndsidorenko@edu.hse.ru>
2025-11-23 20:43:31 +0300

    Added CRUD with forms

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render, redirect, get_object_or_404 
+from .models import Task 
+from .forms import TaskForm
+
 def index(request): 
     return HttpResponse("Привет, это мой первый Django-проект!")
 
@@ -8,3 +10,32 @@ from .models import Task
 def tasks_list(request):
     tasks = Task.objects.all()
     return render(request, "tasks_list.html", {"tasks": tasks})
+
+def task_create(request): 
+        if request.method == "POST": 
+            form = TaskForm(request.POST) 
+            if form.is_valid(): 
+                form.save() 
+                return redirect('tasks_list') 
+        else: 
+            form = TaskForm() 
+            return render(request, "task_form.html", {"form": form}) 
+
+def task_update(request, pk): 
+    task = get_object_or_404(Task, pk=pk) 
+    if request.method == "POST": 
+        form = TaskForm(request.POST, instance=task) 
+        if form.is_valid(): 
+            form.save() 
+            return redirect('tasks_list') 
+    else: 
+
+        form = TaskForm(instance=task) 
+    return render(request, "task_form.html", {"form": form})
+
+def task_delete(request, pk): 
+    task = get_object_or_404(Task, pk=pk) 
+    if request.method == "POST": 
+        task.delete() 
+        return redirect('tasks_list') 
+    return render(request, "task_confirm_delete.html", {"task": task}) 
